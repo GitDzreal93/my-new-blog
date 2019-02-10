19246f287053723bec19949daa0d291b04d99d61
HuangDz <Dzreal_93@126.com>
2019-02-10 23:57:18 +0800

    更改主题

--- conf.py
+++ conf.py
@@ -77,7 +77,8 @@ pygments_style = None
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'defualt'
+html_theme = 'default'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
